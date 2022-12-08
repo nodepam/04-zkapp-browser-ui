a8eeef77b49d12c77f4b4501a4890709aa7cb446
lampam <lampamcrew5@gmail.com>
2022-12-08 07:31:52 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-e4d28c53365a7ed5.js
+++ _next/static/chunks/webpack-e4d28c53365a7ed5.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "4bf89519436b292f" + ".css";
+/******/ 			return "static/css/" + "eed2ba2eeb1ac617" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
